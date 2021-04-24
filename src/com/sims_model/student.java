3c420547c6420fd4a2323a3ae848e035f69414f2
Layan <layanjayasinghe2@gmail.com>
2021-04-24 23:16:36 +0530

    minor changes

--- src/com/sims_model/student.java
+++ src/com/sims_model/student.java
@@ -1,5 +1,56 @@
 package com.sims_model;
 
 public class student {
+	private int sid;
+	private String name;
+	private int age;
+	private String address;
+	private String contact;
+	private int grade;
+	private String className;
+	private String profilePic;
+	
+	public student(int sID, String name, int age, String address, String contact, int grade, String className,
+			String profilePic) {
+		this.sid = sID;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.contact = contact;
+		this.grade = grade;
+		this.className = className;
+		this.profilePic = profilePic;
+	}
 
+	public int getSid() {
+		return sid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public String getProfilePic() {
+		return profilePic;
+	}
 }
